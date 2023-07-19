d438369c8a10af3d6482745cae17487987bde3f3
Gandhisurya <gandhisurya508@gmail.com>
2023-07-19 23:42:50 +0530

    analutics add

--- app/layout.js
+++ app/layout.js
@@ -14,17 +14,19 @@ export default function RootLayout({ children }) {
         <Toaster />
       </body>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-RMKPBEFTJ5"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-RSSKDBNJPZ`}
       />
-      <Script strategy="afterInteractive" id="google-analytics">
+
+      <Script id="ga-script" strategy="lazyOnload">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-RMKPBEFTJ5');
-          `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-RSSKDBNJPZ', {
+      page_path: window.location.pathname,
+    });
+        `}
       </Script>
     </html>
   );
